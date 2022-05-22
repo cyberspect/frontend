1f8577675894aa156d1dc8fd7992f9248761b819
brice-syslogic <65510350+brice-syslogic@users.noreply.github.com>
2022-05-22 11:28:24 -0700

    Moving home_url logic to index.html

--- src/containers/DefaultHeader.vue
+++ src/containers/DefaultHeader.vue
@@ -1,7 +1,7 @@
 <template>
   <AppHeader fixed>
     <SidebarToggler class="d-lg-none" display="md" mobile />
-    <a :href="home_url"><img class="d-lg-inline d-md-none d-sm-none bv-d-xs-down-none" style="margin-left:10px" src="@/assets/img/brand/dt-logo-symbol.svg" width="30" height="30" alt="Dependency-Track Logo"/></a>
+    <a id="home_url"><img class="d-lg-inline d-md-none d-sm-none bv-d-xs-down-none" style="margin-left:10px" src="@/assets/img/brand/dt-logo-symbol.svg" width="30" height="30" alt="Dependency-Track Logo"/></a>
     <SidebarToggler class="d-md-down-none" display="lg" :defaultOpen=true />
     <b-navbar-nav class="ml-auto">
       <DefaultHeaderProfileDropdown/>
@@ -12,10 +12,6 @@
   import { Header as AppHeader, SidebarToggler, AsideToggler } from '@coreui/vue'
   import DefaultHeaderProfileDropdown from './DefaultHeaderProfileDropdown'
 
-  var parts = window.location.hostname.split('.')
-  parts.shift();
-  var home_url = 'https://' + parts.join('.');
-
   export default {
     name: 'DefaultHeader',
     components: {
@@ -23,7 +19,6 @@
       AppHeader,
       DefaultHeaderProfileDropdown,
       SidebarToggler
-    },
-    home_url: home_url
+    }
   }  
 </script>
