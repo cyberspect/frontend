ebd23bb01ccb4e26f594b504b196424c22f3842d
brice-syslogic <65510350+brice-syslogic@users.noreply.github.com>
2022-05-22 14:42:42 -0700

    Hardcoding for now

--- src/containers/DefaultHeader.vue
+++ src/containers/DefaultHeader.vue
@@ -1,7 +1,7 @@
 <template>
   <AppHeader fixed>
     <SidebarToggler class="d-lg-none" display="md" mobile />
-    <a :href="home_url"><img class="d-lg-inline d-md-none d-sm-none bv-d-xs-down-none" style="margin-left:10px" src="@/assets/img/brand/dt-logo-symbol.svg" width="30" height="30" alt="Dependency-Track Logo"/></a>
+    <a href="https://sast.abbott.cyberspect.io/recent_scans/"><img class="d-lg-inline d-md-none d-sm-none bv-d-xs-down-none" style="margin-left:10px" src="@/assets/img/brand/dt-logo-symbol.svg" width="30" height="30" alt="Dependency-Track Logo"/></a>
     <SidebarToggler class="d-md-down-none" display="lg" :defaultOpen=true />
     <b-navbar-nav class="ml-auto">
       <DefaultHeaderProfileDropdown/>
